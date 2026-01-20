8d33240860daf95d12ac0a67125a4a2d51085764
AlexandraKurylo <aleksandrakurilo61@gmail.com>
2026-01-20 13:15:14 +0200

    Add files via upload

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -2,7 +2,21 @@
 
 const header = document.querySelector('.header');
 
-document.addEventListener('click', documentActions)
+document.addEventListener('click', documentActions);
+
+
+window.addEventListener('load', load)
+
+function load() {
+	/* Перевірка мобільного браузера */
+	const isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+	/* Додавання класу touch для HTML, якщо браузер мобільний */
+	function addTouchAttr() {
+		// Додавання data-fls-touch для HTML, якщо браузер мобільний
+		if (isMobile.any()) document.documentElement.setAttribute('data-fls-touch', '')
+	}
+	addTouchAttr()
+}
 
 function documentActions(e) {
 	const targetElement = e.target
